c8d9c59836d2a26c6d3064e7f6424ef87a789716
Hein Wessels <heinwessels93@gmail.com>
2024-10-14 19:43:46 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Larger Display Panels
+
+[![shield](https://img.shields.io/badge/Ko--fi-Donate%20-hotpink?logo=kofi&logoColor=white)](https://ko-fi.com/stringweasel) [![shield](https://img.shields.io/badge/dynamic/json?color=orange&label=Factorio&query=downloads_count&suffix=%20downloads&url=https%3A%2F%2Fmods.factorio.com%2Fapi%2Fmods%2Flarger-display-panels)](https://mods.factorio.com/mod/larger-display-panels)
+
+Larger display panels for your visualization needs. For more information about Display panels see [FFF #419](https://www.factorio.com/blog/post/fff-419).
+
+Adds three new versions:
+- **Medium display panel**: 2x2
+- **Big Display panel**: 4x4 
+- **Giant Display panel**: 6x6
+
+# Credits
+- [Galdoc](https://www.twitch.tv/galdoc) for his mod [Icon Badges](https://mods.factorio.com/mod/icon-badges), which is where I took the letters on the icons from.
+- _justarandomgeek_ for the [Factorio Modding Toolkit](https://marketplace.visualstudio.com/items?itemName=justarandomgeek.factoriomod-debug), without which this mod would not have been possible.
---
+++ data.lua
@@ -0,0 +1,114 @@
+
+local base_entity = data.raw["display-panel"]["display-panel"]
+assert(base_entity)
+local base_item = data.raw["item"]["display-panel"]
+assert(base_item)
+local base_recipe = data.raw["recipe"]["display-panel"]
+assert(base_recipe)
+
+local technology = data.raw["technology"]["circuit-network"]
+
+---@class (exact) PanelConfiguration
+---@field name string
+---@field scale int
+---@field order_postix string
+---@field icon_overlay string
+
+---@param config PanelConfiguration
+---@return data.DisplayPanelPrototype
+---@return data.ItemPrototype
+---@return data.RecipePrototype
+local function make_panel(config)
+    -- ENTITY --------------------
+    local entity = table.deepcopy(base_entity)
+    entity.name = config.name
+
+    if entity.minable then
+        entity.minable.result = config.name
+        entity.minable.mining_time = entity.minable.mining_time * config.scale
+    end
+
+    local ics = entity.icon_draw_specification or { }
+    entity.icon_draw_specification = entity.icon_draw_specification or ics
+    ics.scale = (ics.scale or 1) * config.scale
+    ics.shift = util.mul_shift(ics.shift, config.scale)
+
+    --TODO Corpse
+
+    for _, sprite in pairs(entity.sprites) do
+        for _, layer in pairs(sprite.layers) do -- TODO Not all sprites have layers. Just like ogers.
+            layer.scale = (layer.scale or 1) * config.scale
+            layer.shift = util.mul_shift(layer.shift, config.scale)
+
+            -- Attempt to have the sprites not be compressed in medium quality
+            -- TODO Does this work?
+            layer.flags = layer.flags or { }
+            table.insert(layer.flags, "no-scale")
+        end
+    end
+
+    for _, definition in pairs(entity.circuit_connector) do
+        for _, point in pairs(definition.points) do
+            for colour in pairs(point) do
+                point[colour] = util.mul_shift(point[colour], config.scale)
+            end
+        end
+    end
+
+    local padding = (config.scale - 1) / 2
+    for _, box_type in pairs{"collision_box", "selection_box"} do
+        local box = entity[box_type]
+        box[1][1] = box[1][1] - padding
+        box[1][2] = box[1][2] - padding
+        box[2][1] = box[2][1] + padding
+        box[2][2] = box[2][2] + padding
+    end
+
+    entity.text_shift = util.mul_shift(entity.text_shift, config.scale)
+
+    -- ITEM --------------------
+    local item = table.deepcopy(base_item)
+    item.name = config.name
+    item.place_result = config.name
+    item.order = item.order .. config.order_postix
+    item.icons = {
+        {
+            icon = item.icon,
+            icon_size = item.icon_size
+        },
+        {
+            icon = "__larger-display-panels__/graphics/icons/"..config.icon_overlay..".png",
+            icon_size = 64,
+            scale = 0.7,
+            shift = { -3, -3}
+        }
+    }
+
+    -- RECIPE --------------------
+    local recipe = table.deepcopy(base_recipe)
+    recipe.name = config.name
+    recipe.results[1].name = config.name -- TODO Be smarter
+    recipe.enabled = technology == nil -- Enabled as fallback for now
+    recipe.energy_required = (recipe.energy_required or 0.5) * config.scale
+    for _, ingredient in pairs(recipe.ingredients) do
+        ingredient.amount = ingredient.amount * config.scale * config.scale
+    end
+
+    -- TECHNOLOGY --------------------
+    if technology then
+        -- TODO Add it in the array just after the display-panel
+        -- so it shows nicely in the technology GUI
+        table.insert(technology.effects, {
+            type = "unlock-recipe",
+            recipe = config.name
+        })
+    end
+
+    data:extend{entity, item, recipe}
+    return entity, item, recipe
+end
+
+
+make_panel({name = "medium-display-panel", scale = 2, order_postix="-a", icon_overlay="m"})
+make_panel({name = "big-display-panel",    scale = 4, order_postix="-b", icon_overlay="b"})
+make_panel({name = "giant-display-panel",  scale = 6, order_postix="-c", icon_overlay="g"})
